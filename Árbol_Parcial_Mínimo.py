b55e7cddb6d94159cb4e28ba33af6903be9fd31a
vianneyaguila <a22310212@live.ceti.mx>
2025-06-08 20:19:24 -0600

    Árbol Parcial Mínimo

--- Árbol_Parcial_Mínimo.py
+++ Árbol_Parcial_Mínimo.py
@@ -1,11 +1,12 @@
-import tkinter as tk
-from tkinter import messagebox
-import networkx as nx
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import heapq
+import tkinter as tk # Para crear la interfaz gráfica
+from tkinter import messagebox # Para mostrar mensajes emergentes
+import networkx as nx # Permite crear y trabajar con graficos 
+import matplotlib.pyplot as plt # Para dibujar los gráficos como imágenes  
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #Permite integrarse a matpotlib dentro de la ventana de Tkin
+import heapq #Para manejar una cola de prioridad (usada por Prim)
 
 # Grafo de ejemplo
+#Se define el grafo como un diccionario, donde cada clave es un nodo, y el valor es una lista de tuplas(vecino, peso).
 grafo = {
     'A': [('B', 2), ('C', 3)],
     'B': [('A', 2), ('C', 1), ('D', 1)],
@@ -14,35 +15,35 @@ grafo = {
     'E': [('C', 5), ('D', 1)],
 }
 
-class PrimApp:
-    def __init__(self, root):
+class PrimApp: #Se crea una clase para organizar la aplicación y sus elementos
+    def __init__(self, root): #Método que se ejecuta al crear la aplicación root es la ventana principal
         self.root = root
-        self.root.title("Simulador Algoritmo de Prim")
-        self.graph = nx.Graph()
-        self.create_graph()
-        self.pos = nx.spring_layout(self.graph)
-        self.step = 0
-        self.mst_edges = []
-        self.visited = set()
-        self.edges_queue = [(0, None, 'A')]  # (peso, desde, hasta)
+        self.root.title("Simulador Algoritmo de Prim") #Se guarda la ventana en self.root y se le pone el titulo
+        self.graph = nx.Graph() # Se crea una grafo en networkx
+        self.create_graph() #Se llama a un método (create_graph) para agrgar los nudos y aristas
+        self.pos = nx.spring_layout(self.graph) # Calcula las posiciones visuales de los nodos (diseño automático)
+        self.step = 0 # No se usa directamentepero puede servir para contar pasos
+        self.mst_edges = [] # Lista de aristas agregadas al árbol
+        self.visited = set() # Conjunto de nudos ya visitados
+        self.edges_queue = [(0, None, 'A')]  # (peso, desde, hasta) # Cola de prioridad que guarda las posibles aristas a usar
 
         # Interfaz gráfica
-        self.figure, self.ax = plt.subplots(figsize=(6, 4))
-        self.canvas = FigureCanvasTkAgg(self.figure, master=root)
+        self.figure, self.ax = plt.subplots(figsize=(6, 4)) # Crea un gráfico (figure) con un eje (ax) donde se dibujara el gráfico
+        self.canvas = FigureCanvasTkAgg(self.figure, master=root) #Incrusta el gráfico de matplotlib dentro de la ventana
         self.canvas.get_tk_widget().pack()
 
         self.label_step = tk.Label(root, text="Haz clic en 'Siguiente paso' para iniciar.", font=("Arial", 12))
-        self.label_step.pack(pady=5)
+        self.label_step.pack(pady=5) # Etiqueta que mostrará los mensajes descriptivos en la interfaz
 
         self.next_button = tk.Button(root, text="Siguiente paso", command=self.step_prim)
-        self.next_button.pack(pady=10)
+        self.next_button.pack(pady=10) # Botón que al hacer clic ejecuta el siguiente paso del algoritmo
 
-        self.draw_graph("Esperando para iniciar...")
+        self.draw_graph("Esperando para iniciar...") # Dibuja el gráfico inicial sin ningún arista seleccionado
 
-    def create_graph(self):
+    def create_graph(self): 
         for nodo, vecinos in grafo.items():
             for vecino, peso in vecinos:
-                self.graph.add_edge(nodo, vecino, weight=peso)
+                self.graph.add_edge(nodo, vecino, weight=peso) #Lee el diccionario grafo y crea las aristas en networkx
 
     def draw_graph(self, descripcion=""):
         self.ax.clear()
